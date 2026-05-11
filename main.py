8e4fe97da2c4cb3cc765d002b75935e8c5ef2da8
arthurMalveste <arthurmielemalveste@gmail.com>
2026-05-11 12:05:40 -0300

    web scrapping for a single website

--- main.py
+++ main.py
@@ -7,3 +7,24 @@ import json
 
 driver = webdriver.Chrome()
 
+url = 'https://www.cnnbrasil.com.br/tudo-sobre/inteligencia-artificial/'
+
+driver.get(url)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+driver.implicitly_wait(10)
+
+noticias = soup.find_all('figure')
+
+dados = []
+for noticia in noticias:
+    titulo = noticia.find('h2', class_='text-xl font-bold').text.strip()
+    link = noticia.find('a')['href']
+    dados.append({'titulo': titulo, 'link': link})
+    with open('noticias.json', 'w') as f:
+        json.dump(dados, f, ensure_ascii=False, indent=4)
+
+
+
+driver.quit()
