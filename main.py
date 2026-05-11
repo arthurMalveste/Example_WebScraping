dd3be60c44c9627936c934d26720848031210e1a
arthurMalveste <arthurmielemalveste@gmail.com>
2026-05-11 11:30:19 -0300

    install requirements

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import json
+
+driver = webdriver.Chrome()
+
